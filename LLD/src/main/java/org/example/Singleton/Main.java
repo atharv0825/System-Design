d611b52f628add6756286cc3e4350c90824ccfba
Atharv0825 <atharvbabar044@gmail.com>
2026-03-04 08:54:58 +0530

    initial Commit

---
+++ LLD/src/main/java/org/example/Singleton/Main.java
@@ -0,0 +1,15 @@
+package org.example.Singleton;
+
+public class Main {
+    public static void main(String [] args){
+        Singleton s1 = Singleton.getThisIsObject();
+        System.out.println(s1.hashCode());
+        Singleton s2 = Singleton.getThisIsObject();
+        System.out.println(s2.hashCode());
+
+        Singleton s3 = Singleton.getEagerWay();
+        System.out.println(s3.hashCode());
+        Singleton s4 = Singleton.getEagerWay();
+        System.out.println(s4.hashCode());
+    }
+}
---
+++ LLD/src/main/java/org/example/Singleton/Singleton.java
@@ -0,0 +1,40 @@
+package org.example.Singleton;
+
+public class Singleton {
+
+    private Singleton(){
+    }
+
+    private static Singleton thisIsObject;
+
+//    Lazy way of creating object
+    public static Singleton getThisIsObject() {
+
+        if(thisIsObject == null){
+            thisIsObject = new Singleton();
+        }
+
+        return thisIsObject;
+    }
+//    Eager way of creating object
+    private static Singleton eagerWay = new Singleton();
+
+    public static Singleton getEagerWay(){
+        return eagerWay;
+    }
+
+//    creating thread safe object
+    private static Singleton threadSafeObject = new Singleton();
+
+    public static Singleton getThreadSafeObject(){
+        if(threadSafeObject == null){
+            synchronized (Singleton.class){
+                if(threadSafeObject == null){
+                    threadSafeObject = new Singleton();
+                }
+            }
+        }
+
+        return threadSafeObject;
+    }
+}
